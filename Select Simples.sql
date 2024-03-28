9b25145a1cae89ae249da7c613b988dc7cd2aca6
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-03-28 10:17:56 -0300

    Criacao tabela, insert e select

---
+++ Select Simples.sql
@@ -0,0 +1,13 @@
+--Realizada uma carga com os dados para estudos e tendo como objetivo praticar alguns comandos do Sql.
+
+
+-- Select retornando apenas os 10 primeiros dados de cada tabela
+SELECT TOP 10 * FROM [dbo].[TB_OLIST_CUSTOMERS]
+SELECT TOP 10 * FROM [dbo].[TB_OLIST_GEOLOCATION]
+SELECT TOP 10 * FROM [dbo].[TB_OLIST_ORDER_ITEMS]
+SELECT TOP 10 * FROM [dbo].[TB_OLIST_ORDER_PAYMENTS]
+SELECT TOP 10 * FROM [dbo].[TB_OLIST_ORDER_REVIEWS]
+SELECT TOP 10 * FROM [dbo].[TB_OLIST_ORDERS]
+SELECT TOP 10 * FROM [dbo].[TB_OLIST_PRODUCTS]
+SELECT TOP 10 * FROM [dbo].[TB_OLIST_SELLERS]
+SELECT TOP 10 * FROM [dbo].[TB_PRODUCT_CATEGORY_NAME_TRANSLATION]
